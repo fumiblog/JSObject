674344c6409e3fc77915bbd132f971623976fbc0
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-01-24 21:27:31 +0900

    [Str3-2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,98 @@
 'use strict'
 {
+  //18.数値を操作してみよう
+
+  //17,join()、split()を使ってみよう
+  // const d = [2019, 11, 14];
+  // console.log(d.join('/'));
+  // console.log(d.join(''));
+
+  // const t = '17:08:24';
+  // console.log(t.split(':'));
+  // const [hour, minute, second] = t.split(':');
+  // console.log(hour);
+  // console.log(minute);
+  // console.log(second);
+
+  //16.文字列を操作してみよう
+  // const str = 'hello';
+  // console.log(str.length);
+  // console.log(str.substring(2, 4));
+  // console.log(str[1]);
+
+  //15.変数を代入してみよう
+
+  // let x = 1;
+  // let y = x;
+  // x = 5;
+  // console.log(x);
+  // console.log(y);
+
+  // let x = [1, 2];
+  // let y = x;
+  // x[0] = 5;
+  // console.log(x);
+  // console.log(y);
+
+  // let x = [1, 2];
+  // let y = [...x];
+  // x[0] = 5;
+  // console.log(x);
+  // console.log(y);
+
+
+  //14.Object.keys()を使ってみよう
+  // const keys = Object.keys(point);
+  // keys.forEach(key => {
+  //   console.log(`Key: ${key} Value: ${point[key]}`);
+  // });
+
+  // const points = [
+  //   { x: 30, y: 20 },
+  //   { x: 10, y: 50 },
+  //   { x: 40, y: 40 },
+  // ];
+
+  // console.log(points[1].y);
+
+
+  //13.オブジェクトを操作してみよう
+  // const otherProps = {
+  //   r: 4,
+  //   color: 'red',
+  // };
+
+  // const point = {
+  //   x: 100,
+  //   y: 180,
+  //   ...otherProps,
+  // };
+
+  // const { x, r, ...others } = point;
+  // console.log(x);
+  // console.log(r);
+  // console.log(others);
+
+  //12.プロパティを使ってみよう
+  // const point = {
+  //   x: 100,
+  //   y: 180,
+  // };
+
+  // point.x = 120;
+
+  // point.z = 90;
+  // delete point.y;
+  // console.log(point);
+
+
   //11.objectを作ってみよう
   // const point = [100, 180];
 
-  const point = {
-    x: 100,
-    y: 180,
-  };
-  console.log(point);
+  // const point = {
+  //   x: 100,
+  //   y: 180,
+  // };
+  // console.log(point);
+
 }
