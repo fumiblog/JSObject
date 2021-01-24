1ffcd606d43422db79d3dbfc2be4d96ddcf714bc
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-01-24 11:04:09 +0900

    [First] 1-1

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScriptオブジェクト編
+
+## 2st JavaScript
+
+### ドットインストール　https://dotinstall.com/lessons/basic_javascript_objects_v2
+
+#### 2021.1.24 sunday rain
---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+{
+  const scores = [80, 90, 30, 70];
+
+  // console.log(`Score: ${scores[0]}`);
+  // console.log(`Score: ${scores[1]}`);
+  // console.log(`Score: ${scores[2]}`);
+
+  for (let i = 0; i < scores.length; i++) {
+    console.log(`Score ${i}: ${scores[i]}`);
+  }
+}
